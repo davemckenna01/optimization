0e29c5f8f3dd52c5692f6907a0f6ff76565abf59
Dave McKenna <davemckenna01@gmail.com>
2012-02-21 12:47:47 -0500

    added genetic algorithm

--- optimization.py
+++ optimization.py
@@ -235,3 +235,62 @@ def annealingoptimize(domain, costf, T=10000.0, cool=0.95, step=8):
 
   return vec
 
+
+def geneticoptimize(domain, costf, popsize=50, step=1,
+                    mutprob=0.2, elite=0.2, maxiter=100):
+
+  #Mutation Operation
+  def mutate(vec):
+    #this is something like 0 or 3 or 8
+    i = random.randint(0,len(domain)-1)
+    rand = random.random()
+    if rand < 0.5 and vec[i] > domain[i][0]:
+      return vec[0:i] + [vec[i] - step] + vec[i+1:]
+    elif vec[i] < domain[i][1]:
+      return vec[0:i] + [vec[i] + step] + vec[i+1:]
+    else:
+      return vec[0:i] + [vec[i] - step] + vec[i+1:]
+
+
+  # Crossover Operation
+  def crossover(r1,r2):
+    i=random.randint(1,len(domain)-2)
+    return r1[0:i]+r2[i:]
+
+  # Build the initial population
+  pop=[]
+  for i in range(popsize):
+    vec=[random.randint(domain[i][0],domain[i][1]) 
+         for i in range(len(domain))]
+    pop.append(vec)
+
+  # How many winners from each generation?
+  topelite=int(elite*popsize)
+
+  # Main loop 
+  for i in range(maxiter):
+    scores=[(costf(v),v) for v in pop]
+    scores.sort()
+    ranked=[v for (s,v) in scores]
+
+    # Start with the pure winners
+    pop=ranked[0:topelite]
+
+    # Add mutated and bred forms of the winners
+    while len(pop)<popsize:
+      if random.random()<mutprob:
+
+        # Mutation
+        c=random.randint(0,topelite)
+        pop.append(mutate(ranked[c]))
+      else:
+
+        # Crossover
+        c1=random.randint(0,topelite)
+        c2=random.randint(0,topelite)
+        pop.append(crossover(ranked[c1],ranked[c2]))
+
+    # Print current best score
+    print scores[0][0], scores[0][1]
+
+  return scores[0][1]
